2fd07a759c2dd246d7384f052dbba62edd5974cf
dax <dax26d@gmail.com>
2023-04-21 13:31:39 +0800

    solved problem 3

---
+++ tests/problem03/test.cpp
@@ -0,0 +1,30 @@
+#include "gtest/gtest.h"
+#include "problem.h"
+
+Solution solution;
+
+TEST(EULER03, IS_PRIME)
+{
+	EXPECT_TRUE(solution.is_prime(2));
+	EXPECT_TRUE(solution.is_prime(3));
+	EXPECT_TRUE(solution.is_prime(5));
+	EXPECT_FALSE(solution.is_prime(4));
+	EXPECT_FALSE(solution.is_prime(33));
+}
+
+TEST(EULER03, PRIME_FACTORS)
+{
+	std::vector<int> pf{5, 7, 13, 29};
+	EXPECT_EQ(solution.prime_factors(13195), pf);
+}
+
+TEST(EULER03, LARGEST_PRIME_FACTOR)
+{
+	EXPECT_EQ(solution.largest_prime_factor(13195), 29);
+}
+
+int main(int argc, char *argv[])
+{
+	testing::InitGoogleTest();
+	return RUN_ALL_TESTS();
+}
