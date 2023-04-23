2e659f5572b6a2d867b3149fb8cac027ce8d52f3
dax <dax26d@gmail.com>
2023-04-23 20:32:18 +0800

    solve problem 12

--- tests/problem12/test.cpp
+++ tests/problem12/test.cpp
@@ -3,9 +3,17 @@
 
 Solution solution;
 
+TEST(EULER12, NTH_TRIANLE_NUMBER)
+{
+	EXPECT_EQ(solution.nth_triangle_number(7), 28);
+	EXPECT_EQ(solution.nth_triangle_number(10), 55);
+}
+TEST(EULER12, TRIANGLE_NUMBER_WITH_OVER_N_DIVISOR)
+{
+	EXPECT_EQ(solution.triangle_number_with_over_n_divisors(5), 28);
+}
 int main(int argc, char *argv[])
 {
 	testing::InitGoogleTest();
 	return RUN_ALL_TESTS();
 }
-
