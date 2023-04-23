ecd6726b92c3eb037607241603ad9c44df596826
dax <dax26d@gmail.com>
2023-04-23 16:51:05 +0800

    solved problem 10

---
+++ src/problem10/problem.cpp
@@ -0,0 +1,27 @@
+/*
+Problem description:
+Summation of primes
+Problem 10
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+page link:
+https://projecteuler.net/problem=10
+
+dax 2023-04-23 16:41:30
+*/
+#include "problem.h"
+unsigned long Solution::summation_of_primes(int cap)
+{
+	unsigned long sum{};
+	for(unsigned i = 2; i < cap; ++i)
+	{
+		if(is_prime(i)) sum += i;
+	}
+	return sum;
+}
+
+void Solution::answer()
+{
+	std::cout << "The answer is: " << summation_of_primes(2'000'000) << std::endl;
+}
---
+++ src/problem10/problem.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include "tools.h"
+
+class Solution
+{
+public:
+	unsigned long summation_of_primes(int cap);
+	void answer();
+
+};
+	
