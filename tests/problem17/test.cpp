434a03f7df950dbdeeac9dde7891313b5b66b6b8
dax <dax26d@gmail.com>
2023-04-27 16:42:46 +0800

    solved problem 17

---
+++ tests/problem17/test.cpp
@@ -0,0 +1,27 @@
+#include "gtest/gtest.h"
+#include "problem.h"
+
+Solution solution;
+
+TEST(EULER17, SAY)
+{
+	EXPECT_STREQ(solution.say_number(342).c_str(), "three hundred and forty-two");
+	EXPECT_STREQ(solution.say_number(42).c_str(), "forty-two");
+	EXPECT_STREQ(solution.say_number(115).c_str(), "one hundred and fifteen");
+	EXPECT_STREQ(solution.say_number(300).c_str(), "three hundred");
+	EXPECT_STREQ(solution.say_number(1000).c_str(), "one thousand");
+}
+
+TEST(EULER17, COUNT)
+{
+	EXPECT_EQ(solution.count(342), 23);
+	EXPECT_EQ(solution.count(115), 20);
+	EXPECT_EQ(solution.count(300), 12);
+}
+
+int main(int argc, char *argv[])
+{
+	testing::InitGoogleTest();
+	return RUN_ALL_TESTS();
+}
+
