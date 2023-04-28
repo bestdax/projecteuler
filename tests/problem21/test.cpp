a014699d34c6a8cd9ab5d3d46d41b9d7171ca6f2
dax <dax26d@gmail.com>
2023-04-28 11:46:52 +0800

    solved problem 21

---
+++ tests/problem21/test.cpp
@@ -0,0 +1,24 @@
+#include "gtest/gtest.h"
+#include "problem.h"
+
+Solution solution;
+
+TEST(EULER21, SUM)
+{
+	EXPECT_EQ(solution.sum_of_proper_divisors(220), 284);
+	EXPECT_EQ(solution.sum_of_proper_divisors(284), 220);
+	EXPECT_EQ(solution.sum_of_proper_divisors(16), 15);
+}
+
+TEST(EULER21, AMICABLE)
+{
+	EXPECT_TRUE(solution.has_amicable(220));
+	EXPECT_FALSE(solution.has_amicable(4));
+}
+
+int main(int argc, char *argv[])
+{
+	testing::InitGoogleTest();
+	return RUN_ALL_TESTS();
+}
+
