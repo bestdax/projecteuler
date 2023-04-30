979960342d3974f885ada2e3cf8a75413c6d3282
dax <dax26d@gmail.com>
2023-04-30 09:14:30 +0800

    solved problem 23

--- src/problem23/problem.cpp
+++ src/problem23/problem.cpp
@@ -18,14 +18,61 @@ number that cannot be expressed as the sum of two abundant numbers is less than
 this limit.
 
 Find the sum of all the positive integers which cannot be written as the sum of
-two abundant numbers. page link: https://projecteuler.net/problem=
+two abundant numbers. page link: https://projecteuler.net/problem=23
 
 dax 2023-04-28 14:19:10
 */
 #include "problem.h"
 
-void Solution::answer()
+bool Solution::is_abundant(unsigned n)
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	unsigned sum_of_proper_divisors{};
+	unsigned root = std::round(std::sqrt(n));
+
+	for(unsigned i = 1; i <= root; ++i)
+	{
+		if(n % i == 0)
+		{
+			sum_of_proper_divisors += i;
+			sum_of_proper_divisors += n / i;
+		}
+	}
+
+	sum_of_proper_divisors -= n;
+
+	if(root * root == n) sum_of_proper_divisors -= root;
+
+	return sum_of_proper_divisors > n;
 }
 
+bool Solution::is_sum_of_two_abundants(unsigned n)
+{
+	unsigned middle = n / 2;
+
+	for(unsigned i = 2; i <= middle; ++i)
+	{
+		if(is_abundant(i) && is_abundant(n - i)) return true;
+	}
+
+	return false;
+}
+
+unsigned Solution::non_abundant_sums()
+{
+	unsigned sum{};
+
+	for(unsigned i = 1; i <= 28123; ++i)
+	{
+		if(!is_sum_of_two_abundants(i))
+		{
+			sum += i;
+		}
+	}
+
+	return sum;
+}
+
+void Solution::answer()
+{
+	std::cout << "The answer is: " << non_abundant_sums() << std::endl;
+}
--- src/problem23/problem.h
+++ src/problem23/problem.h
@@ -1,10 +1,13 @@
 #pragma once
 #include <iostream>
+#include <cmath>
 
 class Solution
 {
 public:
+	bool is_abundant(unsigned n);
+	bool is_sum_of_two_abundants(unsigned n);
+	unsigned non_abundant_sums();
 	void answer();
 
 };
-	
