92b7f964beb59a1f92de48fd731f944264b866cb
dax <dax26d@gmail.com>
2023-05-02 14:11:07 +0800

    solved problem 26

---
+++ src/problem26/problem.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Solution
+{
+public:
+	unsigned reciprocal(unsigned n);
+	unsigned number_has_longest_reciprocal_cycles_within(unsigned cap);
+	void answer();
+
+};
