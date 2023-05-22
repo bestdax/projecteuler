dd85555fdb01b811fd2851d9cbba16e26b6d2bc6
dax <dax26d@gmail.com>
2023-05-22 14:51:50 +0800

    solved problem 38

--- tests/problem38/test.cpp
+++ tests/problem38/test.cpp
@@ -3,6 +3,13 @@
 
 Solution solution;
 
+TEST(EULER38, PANDIGITAL_MULTIPLE)
+{
+	EXPECT_EQ(solution.pandigital_multiple(192), 192384576);
+	EXPECT_EQ(solution.pandigital_multiple(9), 918273645);
+
+}
+
 int main(int argc, char *argv[])
 {
 	testing::InitGoogleTest();
