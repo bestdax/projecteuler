40451666e83359581f0d28772294a00047d1f4f5
dax <dax26d@gmail.com>
2023-05-22 16:13:08 +0800

    solved problem40

---
+++ src/problem40/problem.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+class Solution
+{
+public:
+	unsigned nth_digit(unsigned n);
+	unsigned champernowne_constant();
+
+	void answer();
+
+};
---
+++ tests/problem40/test.cpp
@@ -0,0 +1,36 @@
+#include "gtest/gtest.h"
+#include "problem.h"
+
+Solution solution;
+
+TEST(EULER40, NTH_DIGIT)
+{
+  EXPECT_EQ(solution.nth_digit(1), 1);
+  EXPECT_EQ(solution.nth_digit(2), 2);
+  EXPECT_EQ(solution.nth_digit(3), 3);
+  EXPECT_EQ(solution.nth_digit(4), 4);
+  EXPECT_EQ(solution.nth_digit(5), 5);
+  EXPECT_EQ(solution.nth_digit(6), 6);
+  EXPECT_EQ(solution.nth_digit(7), 7);
+  EXPECT_EQ(solution.nth_digit(8), 8);
+  EXPECT_EQ(solution.nth_digit(9), 9);
+  EXPECT_EQ(solution.nth_digit(10), 1);
+  EXPECT_EQ(solution.nth_digit(11), 0);
+  EXPECT_EQ(solution.nth_digit(12), 1);
+  EXPECT_EQ(solution.nth_digit(13), 1);
+  EXPECT_EQ(solution.nth_digit(14), 1);
+  EXPECT_EQ(solution.nth_digit(15), 2);
+  EXPECT_EQ(solution.nth_digit(16), 1);
+  EXPECT_EQ(solution.nth_digit(17), 3);
+  EXPECT_EQ(solution.nth_digit(18), 1);
+  EXPECT_EQ(solution.nth_digit(19), 4);
+  EXPECT_EQ(solution.nth_digit(20), 1);
+  EXPECT_EQ(solution.nth_digit(21), 5);
+}
+
+int main(int argc, char *argv[])
+{
+  testing::InitGoogleTest();
+  return RUN_ALL_TESTS();
+}
+
