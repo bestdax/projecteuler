d125d3b5a83fe8e64ea4d1268adb29cd832aae55
dax <dax26d@gmail.com>
2023-05-22 15:29:47 +0800

    solved problem 39

---
+++ src/problem39/problem.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+
+class Solution
+{
+public:
+	unsigned number_of_right_triangles(unsigned p);
+	unsigned perimeter_with_maximum_solutions();
+	void answer();
+
+};
