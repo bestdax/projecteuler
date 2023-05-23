f470119210d2bde91c405db44d78cbada558e4e9
dax <dax26d@gmail.com>
2023-05-23 16:06:23 +0800

    solved problem 46

---
+++ tests/problem46/test.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+#include "problem.h"
+
+Solution solution;
+
+TEST(EULER46, IS_GOLDBACH_NUMBER)
+{
+	EXPECT_TRUE(solution.is_goldbach_number(9));
+	EXPECT_TRUE(solution.is_goldbach_number(15));
+	EXPECT_TRUE(solution.is_goldbach_number(21));
+	EXPECT_TRUE(solution.is_goldbach_number(25));
+	EXPECT_TRUE(solution.is_goldbach_number(27));
+	EXPECT_TRUE(solution.is_goldbach_number(33));
+}
+
+int main(int argc, char *argv[])
+{
+	testing::InitGoogleTest();
+	return RUN_ALL_TESTS();
+}
