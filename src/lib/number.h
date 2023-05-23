9ade546ec3900ea24aebf36cfc7535216638d9b0
dax <dax26d@gmail.com>
2023-05-23 15:42:08 +0800

    solved problem 45

--- src/lib/number.h
+++ src/lib/number.h
@@ -18,3 +18,6 @@ bool is_n_pandigital(unsigned number);
 bool is_triangle_number(unsigned long number);
 bool is_pentagonal_number(unsigned long number);
 bool is_hexagonal_number(unsigned long number);
+bool is_odd(unsigned long number);
+bool is_even(unsigned long number);
+bool is_square(unsigned long number);
