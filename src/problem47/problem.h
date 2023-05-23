4e3afbc14e1d252976af832e54ef8906e9139c94
dax <dax26d@gmail.com>
2023-05-23 16:46:28 +0800

    solved problem 47

---
+++ src/problem47/problem.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+	unsigned long first_number(unsigned n);
+	void unique(std::vector<unsigned> &vec);
+	void answer();
+
+};
