69aa1bf3fc7c3b8273cec43b38597ee651b90477
dax <dax26d@gmail.com>
2023-05-23 16:06:23 +0800

    solved problem 46

---
+++ src/problem46/problem.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+
+class Solution
+{
+public:
+	bool is_goldbach_number(unsigned long number);
+	unsigned long find_first_not_goldbach_number();
+	void answer();
+
+};
