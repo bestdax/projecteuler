91ab30b1ca956779ce7aca425823f83a5e7d84ef
dax <dax26d@gmail.com>
2023-05-23 14:08:14 +0800

    solved problem 43

---
+++ src/problem43/problem.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Solution
+{
+public:
+	bool is_divisible();
+	unsigned long sum_of_all_divisible();
+	void answer();
+private:
+	std::string digits = "0123456789";
+
+};
