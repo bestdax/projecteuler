f4cf6885836076517314867eecd588eae5bd12c3
dax <dax26d@gmail.com>
2023-05-24 13:52:42 +0800

    solved problem 49

---
+++ src/problem49/problem.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+	std::vector<unsigned long> prime_permutations(unsigned long n);
+	std::vector<unsigned long> pick_arithmetic_sequence(std::vector<unsigned long> numbers);
+	void answer();
+
+};
