0fe03aa2423dde53c41259e80bbffd5b0fecbc5f
dax <dax26d@gmail.com>
2023-05-26 14:22:17 +0800

    solved problem 53

---
+++ src/problem53/problem.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <array>
+
+class Solution
+{
+public:
+	void answer();
+private:
+	std::array<std::array<unsigned long, 101>, 101> combination_matrix{};
+
+
+};
