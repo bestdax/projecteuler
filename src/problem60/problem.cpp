5879f867b6a300866561f410aef601adcf8ddb73
dax <dax26d@gmail.com>
2024-08-30 15:28:36 +0800

    用unordered_set实现了一下，速度没有改善

--- src/problem60/problem.cpp
+++ src/problem60/problem.cpp
@@ -31,6 +31,22 @@ std::set<std::vector<unsigned long>> Solution::find_all_prime_pairs()
 	return pairs;
 }
 
+std::unordered_set<std::vector<unsigned long>, vector_hash, vector_equal> Solution::find_all_prime_pairs_set()
+{
+	std::unordered_set<std::vector<unsigned long>, vector_hash, vector_equal> pairs;
+
+	for(auto it1 = primes.begin(); it1 != primes.end() - 1; ++it1)
+		for(auto it2 = it1 + 1; it2 != primes.end(); ++it2)
+		{
+			if(dax::is_prime(concatenate(*it1, *it2)) && dax::is_prime(concatenate(*it2, *it1)))
+			{
+				pairs.insert({*it1, *it2});
+			}
+		}
+
+	return pairs;
+}
+
 bool Solution::is_prime_set(std::set<std::vector<unsigned long>>& sets, std::vector<unsigned long> set,
                             unsigned long n)
 {
@@ -49,6 +65,23 @@ bool Solution::is_prime_set(std::set<std::vector<unsigned long>>& sets, std::vec
 	return true;
 }
 
+bool Solution::is_prime_set(std::unordered_set<std::vector<unsigned long>, vector_hash, vector_equal>& sets, std::vector<unsigned long> set,
+                            unsigned long n)
+{
+	if(n <= set.back()) return false;
+
+	for(int i = 0 ; i < set.size(); ++i)
+	{
+		auto copy = set;
+		copy.erase(copy.begin() + i);
+
+		copy.push_back(n);
+
+		if(!sets.contains(copy)) return false;
+	}
+
+	return true;
+}
 std::set<std::vector<unsigned long>> Solution::find_all_sets_with_n_members(unsigned n)
 {
 	auto sets = find_all_prime_pairs();
@@ -77,6 +110,33 @@ std::set<std::vector<unsigned long>> Solution::find_all_sets_with_n_members(unsi
 	return sets;
 }
 
+std::unordered_set<std::vector<unsigned long>, vector_hash, vector_equal> Solution::find_all_sets_with_n_members_set(unsigned n)
+{
+	auto sets = find_all_prime_pairs_set();
+
+	while(n > 2)
+	{
+		std::unordered_set<std::vector<unsigned long>, vector_hash, vector_equal> level_sets;
+
+		for(auto& set : sets)
+		{
+			for(auto &p : primes)
+			{
+				if(is_prime_set(sets, set, p))
+				{
+					auto copy = set;
+					copy.push_back(p);
+					level_sets.insert(copy);
+				}
+			}
+		}
+
+		--n;
+		sets = level_sets;
+	}
+
+	return sets;
+}
 void Solution::answer()
 {
 	auto result = find_all_sets_with_n_members(5);
@@ -93,5 +153,22 @@ void Solution::answer()
 	}
 
 	std::cout << "The answer is: " << min_sum << std::endl;
+}
 
+void Solution::answer2()
+{
+	auto result = find_all_sets_with_n_members_set(5);
+	unsigned long min_sum{std::numeric_limits<unsigned long>::max()};
+
+	for(auto&set : result)
+	{
+		auto sum = std::accumulate(set.begin(), set.end(), 0);
+
+		if(sum < min_sum)
+		{
+			min_sum = sum;
+		}
+	}
+
+	std::cout << "The answer is: " << min_sum << std::endl;
 }
--- src/problem60/problem.h
+++ src/problem60/problem.h
@@ -5,6 +5,32 @@
 #include <algorithm>
 #include <limits>
 #include <set>
+#include <unordered_set>
+
+// 自定义哈希函数
+struct vector_hash
+{
+	std::size_t operator()(const std::vector<unsigned long>& v) const
+	{
+		std::size_t hash = 0;
+
+		for(unsigned long elem : v)
+		{
+			hash ^= std::hash<unsigned long> {}(elem) + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+		}
+
+		return hash;
+	}
+};
+
+// 自定义相等比较函数
+struct vector_equal
+{
+	bool operator()(const std::vector<unsigned long>& lhs, const std::vector<unsigned long>& rhs) const
+	{
+		return lhs == rhs;
+	}
+};
 
 class Solution
 {
@@ -13,8 +39,13 @@ class Solution
 
 	public:
 		void answer();
+		void answer2();
 		bool is_prime_set(std::set<std::vector<unsigned long>>&, std::vector<unsigned long>, unsigned long n);
 		std::set<std::vector<unsigned long>> find_all_prime_pairs();
 		std::set<std::vector<unsigned long>> find_all_sets_with_n_members(unsigned n);
+		bool is_prime_set(std::unordered_set<std::vector<unsigned long>, vector_hash, vector_equal>&,
+		                  std::vector<unsigned long>, unsigned long n);
+		std::unordered_set<std::vector<unsigned long>, vector_hash, vector_equal> find_all_prime_pairs_set();
+		std::unordered_set<std::vector<unsigned long>, vector_hash, vector_equal> find_all_sets_with_n_members_set(unsigned n);
 
 };
