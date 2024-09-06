fd922695b04181b80d9a1d6435da00a7c59197a2
dax <dax26d@gmail.com>
2024-09-07 06:25:39 +0800

    添加问题02并完成伪代码算法

---
+++ src/problem02/problem.cpp
@@ -0,0 +1,12 @@
+/*
+Problem page link:
+https://projecteuler.net/problem=2
+
+dax 2024-09-07 05:53:48
+*/
+#include "problem.h"
+
+void Solution::answer()
+{
+	std::cout << "The answer is: " << "" << std::endl;
+}
