f8e1a68b915c13ec7f0c093158012655a401e3f1
dax <dax26d@gmail.com>
2024-09-06 16:58:48 +0800

    添加问题1，并简单设置笔记样式

---
+++ src/problem01/problem.cpp
@@ -0,0 +1,12 @@
+/*
+Problem page link:
+https://projecteuler.net/problem=1
+
+dax 2024-09-06 16:33:46
+*/
+#include "problem.h"
+
+void Solution::answer()
+{
+	std::cout << "The answer is: " << "" << std::endl;
+}
