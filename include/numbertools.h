f2bee636f462bb5e4ac98a6d921c28a87ff8b715
dax <dax26d@gmail.com>
2024-09-08 23:21:26 +0800

    添加欧拉筛法

--- include/numbertools.h
+++ include/numbertools.h
@@ -7,4 +7,5 @@ namespace dax
 bool is_prime(unsigned long);
 bool is_prime_improved(unsigned long);
 std::vector<bool> sieve_of_eratosthenes(unsigned long);
+std::vector<bool> sieve_of_euler(unsigned long);
 }
--- lib/prime.cpp
+++ lib/prime.cpp
@@ -50,8 +50,28 @@ std::vector<bool> dax::sieve_of_eratosthenes(unsigned long n)
 	return prime_status;
 }
 
+std::vector<bool> dax::sieve_of_euler(unsigned long n)
+{
+	std::vector<bool> prime_status(n + 1, true);
+	prime_status[0] = prime_status[1] = false;
+	std::vector<unsigned long> primes;
+	primes.reserve(n / std::log(n));
+
+	for(unsigned long i = 2; i <= n; ++i)
+	{
+		if(prime_status[i]) primes.push_back(i);
+
+		for(auto&p : primes)
+		{
+			auto composite = i * p;
+			if(composite > n) break;
+
+			prime_status[composite] = false;
+
+			if(i % p == 0) break;
+		}
 	}
 
-	return true;
+	return prime_status;
 
 }
