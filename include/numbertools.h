249b9a4f29d7e3f0e49e05a55a3e2bd86bb6a113
dax <dax26d@gmail.com>
2024-09-08 23:20:35 +0800

    添加埃氏筛法

--- include/numbertools.h
+++ include/numbertools.h
@@ -1,7 +1,10 @@
 #pragma once
+#include <vector>
+#include <cmath>
 
 namespace dax
 {
 bool is_prime(unsigned long);
 bool is_prime_improved(unsigned long);
+std::vector<bool> sieve_of_eratosthenes(unsigned long);
 }
--- lib/prime.cpp
+++ lib/prime.cpp
@@ -31,6 +31,25 @@ bool dax::is_prime_improved(unsigned long n)
 		if(n % i == 0 || n % (i + 2) == 0) return false;
 
 		i += 6;
+
+std::vector<bool> dax::sieve_of_eratosthenes(unsigned long n)
+{
+	std::vector<bool> prime_status(n + 1, true);
+	prime_status[0] = prime_status[1] = false;
+	unsigned long root = std::sqrt(n);
+
+	for(unsigned long i = 2; i <= root; ++i)
+	{
+		if(prime_status[i])
+		{
+			for(unsigned long j = i * i; j <= n; j += i)
+				prime_status[j] = false;
+		}
+	}
+
+	return prime_status;
+}
+
 	}
 
 	return true;
