fd71de53f9ae1c5555f1f0c844b03b9a913811cb
dax <dax26d@gmail.com>
2024-09-08 23:25:38 +0800

    添加素数判定的测试

--- tests/lib/prime_test.cpp
+++ tests/lib/prime_test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <numbertools.h>
+#include <timer.h>
 
 TEST(prime, is_prime)
 {
@@ -30,4 +31,38 @@ TEST(prime, is_prime_improved)
 	EXPECT_TRUE(dax::is_prime_improved(97));
 	EXPECT_TRUE(dax::is_prime_improved(101));
 	EXPECT_FALSE(dax::is_prime_improved(91));
+}
+
+void test_prime(unsigned long n)
+{
+	std::vector<bool> prime_status(n + 1, true);
+	for(unsigned long i = 0; i <= n; ++i)
+	{
+	 prime_status[i] = dax::is_prime(i);
+	}
+}
+
+void test_prime_improved(unsigned long n)
+{
+	std::vector<bool> prime_status(n + 1, true);
+	for(unsigned long i = 1; i <= n; ++i)
+	{
+		prime_status[i] = dax::is_prime_improved(i);
+	}
+}
+
+TEST(prime, primer_timer)
+{
+	measure_exe_time(test_prime, 1e6);
+	measure_exe_time(test_prime_improved, 1e6);
+	measure_exe_time(dax::sieve_of_eratosthenes, 1e6);
+	measure_exe_time(dax::sieve_of_euler, 1e6);
+
+	for(unsigned long i = 1; i < 1e6; ++i)
+	{
+		if(dax::is_prime(i) != dax::is_prime_improved(i))
+			std::cout << i << std::endl;
+	}
+
+	EXPECT_TRUE(dax::sieve_of_eratosthenes(1e6) == dax::sieve_of_euler(1e6));
 }
