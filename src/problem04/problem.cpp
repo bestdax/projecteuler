0f0e15e2e5aafb9b83666627eeefc35d9a306af8
dax <dax26d@gmail.com>
2024-09-09 13:57:40 +0800

    完成问题04

--- src/problem04/problem.cpp
+++ src/problem04/problem.cpp
@@ -5,8 +5,22 @@ https://projecteuler.net/problem=4
 dax 2024-09-09 11:32:14
 */
 #include "problem.h"
+#include <numutils.h>
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	unsigned max_palindrome{};
+
+	for(unsigned i = 100; i < 1000; ++i)
+		for(unsigned j = 100; j < 1000; ++j)
+		{
+			unsigned product = i * j;
+
+			if(dax::is_palindrome(product) && product > max_palindrome)
+			{
+				max_palindrome = product;
+			}
+		}
+
+	std::cout << "The answer is: " << max_palindrome << std::endl;
 }
