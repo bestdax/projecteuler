bad7fdb6b9f33c86a6401fcae661b92accfdd9e2
dax <dax26d@gmail.com>
2024-09-09 17:53:14 +0800

    完成问题09

--- src/problem09/problem.cpp
+++ src/problem09/problem.cpp
@@ -8,5 +8,16 @@ dax 2024-09-09 17:30:05
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	for(unsigned c = 334; c < 500; ++c)
+		for(unsigned a = 1; a < c; ++a)
+		{
+			auto b = 1000 - a - c;
+
+			if(a * a + b * b == c * c)
+			{
+				std::cout << "The answer is: " << a * b * c << std::endl;
+				return;
+			}
+		}
+
 }
