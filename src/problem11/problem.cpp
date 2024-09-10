3b4709bbb89574a4bced70cfd1f8b3de316c4d59
dax <dax26d@gmail.com>
2024-09-10 09:42:00 +0800

    完成问题11

--- src/problem11/problem.cpp
+++ src/problem11/problem.cpp
@@ -8,5 +8,31 @@ dax 2024-09-10 08:48:37
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	unsigned long max_product{1};
+
+	for(unsigned col = 0; col < 20; ++col)
+		for(unsigned row = 0; row < 20; ++row)
+		{
+			if(col < 17 && row < 17)
+			{
+				auto product_h = matrix[row][col] * matrix[row][col + 1] * matrix[row][col + 2] * matrix[row][col + 3];
+				auto product_v = matrix[row][col] * matrix[row + 1][col] * matrix[row + 2][col] * matrix[row + 3][col];
+				auto product_d = matrix[row][col] * matrix[row + 1][col + 1] * matrix[row + 2][col + 2] * matrix[row + 3][col + 3];
+
+				for(auto &p :
+				        {
+				            product_h, product_v, product_d
+				        })
+					max_product = p > max_product ? p : max_product;
+			};
+
+			if(col > 2 && row < 17)
+			{
+				auto product_ad = matrix[row][col] * matrix[row + 1][col - 1] * matrix[row + 2][col - 2] * matrix[row + 3][col - 3];
+				max_product = product_ad > max_product ? product_ad : max_product;
+			}
+
+		}
+
+	std::cout << "The answer is: " << max_product << std::endl;
 }
