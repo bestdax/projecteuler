6f7472d0a97f681fc2dc1ae3c993fb766b9e51bf
dax <dax26d@gmail.com>
2024-09-10 12:00:35 +0800

    完成问题13

--- src/problem13/problem.cpp
+++ src/problem13/problem.cpp
@@ -5,8 +5,14 @@ https://projecteuler.net/problem=13
 dax 2024-09-10 10:15:03
 */
 #include "problem.h"
+#include <stringint.h>
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	StringInt result;
+	for(auto& sn: string_numbers)
+	{
+		result = result + sn;
+	}
+	std::cout << "The answer is: " << result.to_string().substr(0, 10) << std::endl;
 }
