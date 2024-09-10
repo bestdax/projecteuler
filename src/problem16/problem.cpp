56cdd005fefc9b79975f6859df36b0bf42b71e90
dax <dax26d@gmail.com>
2024-09-10 14:11:23 +0800

    完成问题16

--- src/problem16/problem.cpp
+++ src/problem16/problem.cpp
@@ -5,8 +5,9 @@ https://projecteuler.net/problem=16
 dax 2024-09-10 13:00:35
 */
 #include "problem.h"
+#include <stringint.h>
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	std::cout << "The answer is: " << StringInt(2).power(1000).sum_of_digits() << std::endl;
 }
