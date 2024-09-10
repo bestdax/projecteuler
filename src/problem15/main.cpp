e9f6198b8ab6cfc44a6b5e26d3e5eeceb76bc0a7
dax <dax26d@gmail.com>
2024-09-10 12:49:46 +0800

    添加问题15

---
+++ src/problem15/main.cpp
@@ -0,0 +1,14 @@
+#include "problem.h"
+#include "timer.h"
+
+int main(int argc, char *argv[])
+{
+	Solution solution;
+	auto func = [&solution]()
+	{
+		solution.answer();
+	};
+
+	measure_exe_time(func);
+	return 0;
+}
---
+++ src/problem15/problem.cpp
@@ -0,0 +1,12 @@
+/*
+Problem page link:
+https://projecteuler.net/problem=15
+
+dax 2024-09-10 12:23:27
+*/
+#include "problem.h"
+
+void Solution::answer()
+{
+	std::cout << "The answer is: " << "" << std::endl;
+}
