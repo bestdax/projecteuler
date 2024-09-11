ac0d0e891f8cab5116d2f7c13bf142bfbbf7b270
dax <dax26d@gmail.com>
2024-09-11 11:11:34 +0800

    完成问题19

--- src/problem19/problem.cpp
+++ src/problem19/problem.cpp
@@ -6,7 +6,32 @@ dax 2024-09-11 10:12:31
 */
 #include "problem.h"
 
+bool Solution::is_leap_year(int y)
+{
+	return y % 400 == 0 || y % 4 == 0 && y % 100 != 0;
+}
+
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	unsigned weekday = 1;
+
+	unsigned year = 1900;
+	unsigned count{};
+
+	while(year <= 2000)
+	{
+		for(int m = 1; m <= 12; ++m)
+		{
+			if(m == 12) ++year;
+
+			if(is_leap_year(year) && m == 2) weekday += 29;
+			else weekday += days_of_month[m];
+
+			weekday %= 7;
+
+			if(year > 1900 && weekday == 0) ++count;
+		}
+	}
+
+	std::cout << "The answer is: " << count << std::endl;
 }
--- src/problem19/problem.h
+++ src/problem19/problem.h
@@ -1,9 +1,27 @@
 #pragma once
 #include <iostream>
+#include <map>
 
 class Solution
 {
+	private:
+		std::map<unsigned, unsigned> days_of_month =
+		{
+			{1, 31},
+			{3, 31},
+			{5, 31},
+			{7, 31},
+			{8, 31},
+			{10, 31},
+			{12, 31},
+			{4, 30},
+			{6, 30},
+			{9, 30},
+			{11, 30},
+			{2, 28}
+		};
 	public:
+		bool is_leap_year(int y);
 		void answer();
 
 };
