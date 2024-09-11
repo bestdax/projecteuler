b3fe66570cc1391fa205141f30b6eab6fbe0b7e1
dax <dax26d@gmail.com>
2024-09-11 11:40:34 +0800

    添加问题21

---
+++ src/problem21/problem.cpp
@@ -0,0 +1,12 @@
+/*
+Problem page link:
+https://projecteuler.net/problem=21
+
+dax 2024-09-11 11:39:05
+*/
+#include "problem.h"
+
+void Solution::answer()
+{
+	std::cout << "The answer is: " << "" << std::endl;
+}
---
+++ src/problem21/problem.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+
+class Solution
+{
+	public:
+		void answer();
+
+};
