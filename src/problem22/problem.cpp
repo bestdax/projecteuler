4002daa4e80a0d0f1c83e9219b7dc80874d4e15c
dax <dax26d@gmail.com>
2024-09-11 14:42:11 +0800

    完成问题22

--- src/problem22/problem.cpp
+++ src/problem22/problem.cpp
@@ -5,8 +5,46 @@ https://projecteuler.net/problem=22
 dax 2024-09-11 12:19:51
 */
 #include "problem.h"
+#include <filesystem>
+#include <fstream>
+#include <algorithm>
+
+Solution::Solution()
+{
+	names.reserve(6000);
+	std::filesystem::path p = __FILE__;
+	p.replace_filename("0022_names.txt");
+
+	std::ifstream file(p);
+	std::string name;
+
+	while(std::getline(file, name, ','))
+	{
+		names.push_back(name.substr(1, name.size() - 2));
+	}
+
+	std::sort(names.begin(), names.end());
+
+}
+
+unsigned Solution::name_score(std::string& name)
+{
+	unsigned score{};
+
+	for(auto& c : name)
+		score += c - 64;
+
+	return score;
+}
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	unsigned long sum{};
+
+	for(unsigned i = 0; i < names.size(); ++i)
+	{
+		sum += (i + 1) * name_score(names[i]);
+	}
+
+	std::cout << "The answer is: " << sum << std::endl;
 }
--- src/problem22/problem.h
+++ src/problem22/problem.h
@@ -1,9 +1,15 @@
 #pragma once
 #include <iostream>
+#include <string>
+#include <vector>
 
 class Solution
 {
+	private:
+		std::vector<std::string> names;
 	public:
+		Solution();
+		unsigned name_score(std::string& name);
 		void answer();
 
 };
