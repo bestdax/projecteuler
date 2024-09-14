00785be3f5003d4a47157197e653f1aa444b83eb
dax <dax26d@gmail.com>
2024-09-14 09:25:50 +0800

    完成问题29

--- src/problem29/main.cpp
+++ src/problem29/main.cpp
@@ -9,6 +9,12 @@ int main(int argc, char *argv[])
 		solution.answer();
 	};
 
+	auto func_bint = [&solution]()
+	{
+		solution.answer_bint();
+	};
+
 	measure_exe_time(func);
+	measure_exe_time(func_bint);
 	return 0;
 }
--- src/problem29/problem.cpp
+++ src/problem29/problem.cpp
@@ -5,8 +5,42 @@ https://projecteuler.net/problem=29
 dax 2024-09-12 12:31:21
 */
 #include "problem.h"
+#include <vector>
+#include <stringint.h>
+#include <algorithm>
+#include <bigint.h>
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	std::vector<StringInt> powers;
+
+	for(StringInt a = 2; a <= 100; ++a)
+		for(unsigned b = 2; b <= 100; ++b)
+		{
+			powers.push_back(a.power(b));
+		}
+
+	std::sort(powers.begin(), powers.end());
+	auto last = std::unique(powers.begin(), powers.end());
+	powers.erase(last, powers.end());
+
+	std::cout << "The answer is: " << powers.size() << std::endl;
+}
+
+void Solution::answer_bint()
+{
+
+	std::vector<BigUInt> powers;
+
+	for(BigUInt a = 2; a <= 100; ++a)
+		for(unsigned b = 2; b <= 100; ++b)
+		{
+			powers.push_back(a.power(b));
+		}
+
+	std::sort(powers.begin(), powers.end());
+	auto last = std::unique(powers.begin(), powers.end());
+	powers.erase(last, powers.end());
+
+	std::cout << "The answer is: " << powers.size() << std::endl;
 }
