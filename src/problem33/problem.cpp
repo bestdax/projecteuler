6e70baacd5552972557d048df1040e2cc197eb8a
dax <dax26d@gmail.com>
2024-09-14 17:25:32 +0800

    添加问题33

---
+++ src/problem33/problem.cpp
@@ -0,0 +1,12 @@
+/*
+Problem page link:
+https://projecteuler.net/problem=33
+
+dax 2024-09-14 14:30:04
+*/
+#include "problem.h"
+
+void Solution::answer()
+{
+	std::cout << "The answer is: " << "" << std::endl;
+}
