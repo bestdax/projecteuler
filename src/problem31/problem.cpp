e283a7f9f090f43ca49c42558250bdc0cbb8c297
dax <dax26d@gmail.com>
2024-09-14 12:24:44 +0800

    完成问题31

--- src/problem31/problem.cpp
+++ src/problem31/problem.cpp
@@ -5,8 +5,18 @@ https://projecteuler.net/problem=31
 dax 2024-09-14 11:57:55
 */
 #include "problem.h"
+Solution::Solution()
+{
+	dp[0] = 1;
+};
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	for(auto& coin : coins)
+	{
+		for(unsigned i = coin; i <= 200; ++i)
+			dp[i] += dp[i - coin];
+	}
+
+	std::cout << "The answer is: " << dp[200] << std::endl;
 }
--- src/problem31/problem.h
+++ src/problem31/problem.h
@@ -3,7 +3,11 @@
 
 class Solution
 {
+	private:
+	unsigned dp[201] = {0};
+		unsigned coins[8] {1, 2, 5, 10, 20, 50, 100, 200};
 	public:
+		Solution();
 		void answer();
 
 };
