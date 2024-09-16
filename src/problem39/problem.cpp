a3f2734047e836200ecfc098049d6df76fbf8e73
dax <dax26d@gmail.com>
2024-09-16 15:22:02 +0800

    完成问题39

--- src/problem39/problem.cpp
+++ src/problem39/problem.cpp
@@ -8,5 +8,30 @@ dax 2024-09-16 15:08:36
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	unsigned max{};
+	unsigned max_p{};
+
+	for(unsigned p = 12; p <= 1000; ++p)
+	{
+		unsigned count{};
+
+		for(unsigned a = 1; a < p / 2; ++a)
+			for(unsigned b = a; (p - a) / 2; ++b)
+			{
+				auto c = p - a - b;
+
+				if(c < a || c < b) break;
+
+				if(c * c == b * b + a * a)
+					++count;
+			}
+
+		if(count > max)
+		{
+			max = count;
+			max_p = p;
+		}
+	}
+
+	std::cout << "The answer is: " << max_p << std::endl;
 }
