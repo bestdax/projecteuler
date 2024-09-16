0bdd9b83efbece9636c5f0812cd0c6f8acc9521e
dax <dax26d@gmail.com>
2024-09-16 15:02:36 +0800

    完成问题38

--- src/problem38/problem.cpp
+++ src/problem38/problem.cpp
@@ -5,8 +5,42 @@ https://projecteuler.net/problem=38
 dax 2024-09-16 14:24:46
 */
 #include "problem.h"
+#include <algorithm>
+#include <string>
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	std::string ns = "987654321";
+
+	do
+	{
+		// 每次循环从左边多取一个数字
+		for(int i = 1; i < 5; ++i)
+		{
+			unsigned first_seg = std::stoul(ns.substr(0, i));
+
+			auto seg_start_index = i;
+			unsigned n = 2;
+			std::string calculated_ns = std::to_string(first_seg);
+
+			while(calculated_ns.size() < ns.size())
+			{
+				calculated_ns += std::to_string(first_seg * n);
+
+				if(!ns.starts_with(calculated_ns)) break;
+
+				++n;
+
+			}
+
+			if(calculated_ns == ns)
+			{
+				std::cout << "The answer is: " << ns << std::endl;
+				return;
+			}
+		}
+
+	}
+	while(std::prev_permutation(ns.begin(), ns.end()));
+
 }
