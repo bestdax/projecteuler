72469c578ef1e64a94f27cbd332027283ad1d748
dax <dax26d@gmail.com>
2024-09-16 10:13:02 +0800

    完成问题34

--- src/problem34/problem.cpp
+++ src/problem34/problem.cpp
@@ -5,8 +5,36 @@ https://projecteuler.net/problem=34
 dax 2024-09-16 09:12:37
 */
 #include "problem.h"
+#include <numutils.h>
+#include <vector>
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	unsigned long sum{};
+
+	std::vector<unsigned> factorials(10);
+
+	for(int i = 0; i < 10; ++i)
+	{
+		factorials[i] = dax::factorial(i);
+	}
+
+	unsigned limit = 7 * factorials[9];
+
+	for(unsigned long i = 3; i <= limit; ++i)
+	{
+		unsigned long n = i;
+		unsigned digital_factorial_sum {};
+
+		while(n)
+		{
+			digital_factorial_sum += factorials[n % 10];
+			n /= 10;
+		}
+
+		if(digital_factorial_sum == i) sum += i;
+
+	}
+
+	std::cout << "The answer is: " << sum << std::endl;
 }
