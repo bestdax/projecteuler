c2850f38a71a6f85b16e46c56f266a8d7ff9415a
dax <dax26d@gmail.com>
2024-09-16 11:22:33 +0800

    完成问题36

--- src/problem36/problem.cpp
+++ src/problem36/problem.cpp
@@ -5,8 +5,17 @@ https://projecteuler.net/problem=36
 dax 2024-09-16 11:03:11
 */
 #include "problem.h"
+#include <numutils.h>
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	unsigned sum{};
+
+	for(unsigned i = 0; i < 1e6; ++i)
+	{
+		if(dax::is_palindrome(i) && dax::is_palindrome(i, 2))
+			sum += i;
+	}
+
+	std::cout << "The answer is: " << sum << std::endl;
 }
