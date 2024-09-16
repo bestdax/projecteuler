c8e80c2a92e2494c7222bc74219ec424c77c4c70
dax <dax26d@gmail.com>
2024-09-16 14:21:02 +0800

    完成问题37

--- src/problem37/problem.cpp
+++ src/problem37/problem.cpp
@@ -6,7 +6,47 @@ dax 2024-09-16 13:55:00
 */
 #include "problem.h"
 
+Solution::Solution()
+{
+	is_prime = dax::sieve_of_eratosthenes(1e6);
+}
+
+bool Solution::is_truncatable_prime(unsigned n)
+{
+	if(n < 10 || !is_prime[n]) return false;
+
+	unsigned base = 10;
+
+	while(n % base != n)
+	{
+		// 从右边取
+		if(!is_prime[n % base]) return false;
+
+		// 从左边取
+		if(!is_prime[n / base]) return false;
+
+		base *= 10;
+	}
+
+	return true;
+}
+
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	unsigned count{};
+	unsigned n = 10;
+	unsigned sum{};
+
+	while(count != 11)
+	{
+		if(is_truncatable_prime(n))
+		{
+			++count;
+			sum += n;
+		}
+
+		++n;
+	}
+
+	std::cout << "The answer is: " << sum << std::endl;
 }
--- src/problem37/problem.h
+++ src/problem37/problem.h
@@ -1,9 +1,14 @@
 #pragma once
 #include <iostream>
+#include <prime.h>
 
 class Solution
 {
+	private:
+		std::vector<bool> is_prime;
 	public:
+		Solution();
+		bool is_truncatable_prime(unsigned n);
 		void answer();
 
 };
