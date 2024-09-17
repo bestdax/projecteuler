42cebe9b1f0abbcffddd88c445e4847254bc51f0
dax <dax26d@gmail.com>
2024-09-18 00:07:55 +0800

    完成问题42

--- src/problem42/problem.cpp
+++ src/problem42/problem.cpp
@@ -5,8 +5,61 @@ https://projecteuler.net/problem=42
 dax 2024-09-17 23:28:30
 */
 #include "problem.h"
+#include <filesystem>
+#include <fstream>
+
+void Solution::read_names()
+{
+	std::filesystem::path p = __FILE__;
+	p.replace_filename("0042_words.txt");
+
+	std::ifstream file(p);
+	std::string name;
+
+	while(std::getline(file, name, ','))
+	{
+		max_length = max_length > name.size() - 2 ? max_length : name.size() - 2;
+		names.push_back(name.substr(1, name.size() - 2));
+	}
+}
+
+void Solution::gen_triangles()
+{
+	auto limit = max_length * 26;
+	unsigned n = 1;
+	unsigned triangle = 1;
+
+	while(triangle < limit)
+	{
+		++n;
+		triangle = n * (n + 1) / 2;
+		triangles.insert(triangle);
+
+	}
+}
+
+unsigned Solution::name_score(std::string& name)
+{
+	unsigned score{};
+
+	for(auto&c : name)
+	{
+		score += c - 64;
+	}
+
+	return score;
+}
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	read_names();
+	gen_triangles();
+
+	unsigned count{};
+	for(auto& name: names)
+	{
+		if(triangles.contains(name_score(name)))
+			++count;
+	}
+	std::cout << "The answer is: " << count << std::endl;
 }
--- src/problem42/problem.h
+++ src/problem42/problem.h
@@ -1,9 +1,18 @@
 #pragma once
 #include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_set>
 
 class Solution
 {
+		std::vector<std::string> names;
+		unsigned max_length{};
+		std::unordered_set<unsigned> triangles{1};
 	public:
+		void read_names();
+		void gen_triangles();
+		unsigned name_score(std::string&);
 		void answer();
 
 };
