2c40e241fe5340733082211ed55e58d75f7166b5
dax <dax26d@gmail.com>
2024-09-17 23:24:49 +0800

    完成问题41

--- src/problem41/problem.cpp
+++ src/problem41/problem.cpp
@@ -5,8 +5,22 @@ https://projecteuler.net/problem=41
 dax 2024-09-17 23:03:50
 */
 #include "problem.h"
+#include <prime.h>
+#include <algorithm>
+#include <string>
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	std::string pandigit = "7654321";
+
+	do
+	{
+		if(dax::is_prime_improved(std::stoul(pandigit)))
+		{
+			std::cout << "The answer is: " << pandigit << std::endl;
+			break;
+		}
+	}
+	// 并没有考虑在7位数时找不到的情况
+	while(std::prev_permutation(pandigit.begin(), pandigit.end()));
 }
