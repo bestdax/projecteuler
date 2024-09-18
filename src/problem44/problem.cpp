802161ed71b9852d4ab0ed8203567224afbc5f65
dax <dax26d@gmail.com>
2024-09-18 16:14:12 +0800

    完成问题44

--- src/problem44/problem.cpp
+++ src/problem44/problem.cpp
@@ -5,8 +5,37 @@ https://projecteuler.net/problem=44
 dax 2024-09-18 14:51:32
 */
 #include "problem.h"
+#include <numutils.h>
+#include <utils.h>
+
+Solution::Solution()
+{
+	pentagonals.reserve(1e6);
+}
+
+void Solution::next_pentagonal()
+{
+	++n;
+	pentagonals.push_back(n * (3 * n - 1) / 2);
+}
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	while(true)
+	{
+		next_pentagonal();
+
+		if(pentagonals.size() > 1)
+		{
+			for(auto it = pentagonals.rbegin() + 1;  it != pentagonals.rend(); ++it)
+			{
+				if(dax::is_pentagonal(pentagonals.back() - *it) && dax::is_pentagonal(*it + pentagonals.back()))
+				{
+					std::cout << "The answer is " << pentagonals.back() - *it << std::endl;;
+					return;
+				}
+			}
+		}
+	}
+
 }
--- src/problem44/problem.h
+++ src/problem44/problem.h
@@ -1,9 +1,16 @@
 #pragma once
 #include <iostream>
+#include <vector>
 
 class Solution
 {
+	private:
+		std::vector<unsigned long> pentagonals;
+		unsigned n {};
+
 	public:
+		Solution();
+		void next_pentagonal();
 		void answer();
 
 };
