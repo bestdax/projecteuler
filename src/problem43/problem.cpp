5993ab47624b5e3efd6b15438110ac3eed277630
dax <dax26d@gmail.com>
2024-09-18 11:11:02 +0800

    完成问题43 - 暴力枚举法

--- src/problem43/problem.cpp
+++ src/problem43/problem.cpp
@@ -5,8 +5,30 @@ https://projecteuler.net/problem=43
 dax 2024-09-18 10:03:00
 */
 #include "problem.h"
+#include <algorithm>
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	unsigned long sum{};
+
+	do
+	{
+		bool all_divisible = true;
+
+		for(int i = 0; i < divisors.size(); ++i)
+		{
+			auto seg = pandigit.substr(i + 1, 3);
+
+			if(std::stoul(seg) % divisors[i] != 0)
+			{
+				all_divisible = false;
+				break;
+			}
+		}
+
+		if(all_divisible) sum += std::stoul(pandigit);
+	}
+	while(std::next_permutation(pandigit.begin(), pandigit.end()));
+
+	std::cout << "The answer is: " << sum << std::endl;
 }
--- src/problem43/problem.h
+++ src/problem43/problem.h
@@ -1,8 +1,13 @@
 #pragma once
 #include <iostream>
+#include <vector>
+#include <string>
 
 class Solution
 {
+	private:
+		std::vector<unsigned> divisors{2, 3, 5, 7, 11, 13, 17};
+		std::string pandigit = "1023456789";
 	public:
 		void answer();
 
