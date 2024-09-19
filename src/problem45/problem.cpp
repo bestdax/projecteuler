bf646ad395651ad671a76fa232eb7fcb135b353f
dax <dax26d@gmail.com>
2024-09-19 12:00:41 +0800

    完成问题45

--- src/problem45/problem.cpp
+++ src/problem45/problem.cpp
@@ -5,8 +5,22 @@ https://projecteuler.net/problem=45
 dax 2024-09-19 11:11:23
 */
 #include "problem.h"
+#include <numutils.h>
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	unsigned n = 143;
+
+	while(true)
+	{
+		++n;
+		unsigned long h = n * (2 * n - 1);
+
+		if(dax::is_triangle(h) && dax::is_pentagonal(h))
+		{
+			std::cout << "The answer is: " << h << std::endl;
+			return;
+		}
+	}
+
 }
