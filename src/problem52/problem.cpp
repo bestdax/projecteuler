a75006ff029a0f0403d1922d326950f9bf5cc9f1
dax <dax26d@gmail.com>
2024-09-23 16:56:25 +0800

    完成问题52

--- src/problem52/problem.cpp
+++ src/problem52/problem.cpp
@@ -5,8 +5,39 @@ https://projecteuler.net/problem=52
 dax 2024-09-23 16:35:12
 */
 #include "problem.h"
+#include <string>
+#include <algorithm>
+
+bool has_same_digits(unsigned long a, unsigned long b)
+{
+	auto nsa = std::to_string(a);
+	std::sort(nsa.begin(), nsa.end());
+	auto nsb = std::to_string(b);
+	std::sort(nsb.begin(), nsb.end());
+
+	return nsa == nsb;
+}
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+
+	for(unsigned long n = 2; n < 1e6; ++n)
+	{
+		bool found = true;
+
+		for(unsigned i = 2; i < 6; ++i)
+		{
+			if(!has_same_digits(n, n * i))
+			{
+				found = false;
+				break;
+			}
+		}
+
+		if(found)
+		{
+			std::cout << "The answer is: " << n << std::endl;
+			return;
+		}
+	}
 }
