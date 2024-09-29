ceb08db0175f721b3e0ec26ea02d0f49902d4da0
dax <dax26d@gmail.com>
2024-09-29 14:09:58 +0800

    修正main函数中测时函数中的错误

--- src/problem60/main.cpp
+++ src/problem60/main.cpp
@@ -7,24 +7,17 @@ int main(int argc, char *argv[])
 	Solution solution;
 	auto func = [&solution]()
 	{
-			auto func = [&solution]()
-	{
 		return solution.answer();
 	};
 
 	measure_exe_time(func);
-	};
 
 	auto func2 = [&solution]()
 	{
-			auto func = [&solution]()
-	{
 		return solution.answer();
 	};
 
-	measure_exe_time(func);
-	};
-	measure_exe_time(func);
 	measure_exe_time(func2);
+
 	return 0;
 }
