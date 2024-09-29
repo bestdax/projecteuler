8d40e6818743d913267a31644912e4cb49b4a4c6
dax <dax26d@gmail.com>
2024-09-29 15:27:03 +0800

    完成问题60

--- src/problem60/problem.cpp
+++ src/problem60/problem.cpp
@@ -5,8 +5,79 @@ https://projecteuler.net/problem=60
 dax 2024-09-26 12:49:33
 */
 #include "problem.h"
+#include <prime.h>
+#include <vector>
+#include <numutils.h>
+#include <numeric>
+#include <utils.h>
+
+bool Solution::is_prime_pair(unsigned long a, unsigned long b)
+{
+	return dax::is_prime_improved(dax::concatenate(a, b)) && dax::is_prime_improved(dax::concatenate(b, a));
+}
+
+bool Solution::is_prime_group(std::vector<unsigned long> group)
+{
+	if(group.empty()) return true;
+
+	if(group.size() == 1 && dax::is_prime_improved(group.back())) return true;
+
+	for(unsigned i = 0; i < group.size() - 1; ++i)
+		for(unsigned j = i + 1; j < group.size(); ++j)
+		{
+			if(not group_set.contains({group[i], group[j]})) return false;
+		}
+
+	return true;
+}
+
+void Solution::init_groups()
+{
+	for(unsigned long i = 3; i < limit; ++i)
+		for(unsigned long j = i + 1; j <= limit; ++j)
+		{
+			if(is_prime[i] && is_prime[j] && is_prime_pair(i, j))
+			{
+				group_set.insert({i, j});
+				prime_groups.push_back({i, j});
+			}
+		}
+}
 
 void Solution::answer()
 {
-	std::cout << "The answer is: " << "" << std::endl;
+	is_prime = dax::sieve_of_eratosthenes(limit);
+	init_groups();
+	std::set<unsigned long> sum_set;
+
+	for(unsigned loop = 0; loop < 3; ++loop)
+	{
+		std::vector<std::vector<unsigned long>> temp_groups;
+
+		for(const auto& group : prime_groups)
+		{
+			for(unsigned long i = group.back() + 1; i <= limit; ++i)
+			{
+				if(is_prime[i])
+				{
+					auto copy(group);
+					copy.push_back(i);
+
+					if(is_prime_group(copy))
+					{
+						temp_groups.push_back(copy);
+						if(copy.size() == 5)
+						{
+							sum_set.insert(std::accumulate(copy.begin(), copy.end(), 0));
+						}
+					}
+
+				}
+			}
+		}
+
+		prime_groups = temp_groups;
+	}
+
+	std::cout << "The answer is: " << *sum_set.begin() << std::endl;
 }
--- src/problem60/problem.h
+++ src/problem60/problem.h
@@ -1,9 +1,44 @@
 #pragma once
 #include <iostream>
+#include <vector>
+#include <prime.h>
+#include <unordered_set>
+#include <utility>
+#include <set>
+
+namespace std
+{
+template <>
+struct hash<std::pair<unsigned long, unsigned long>>
+{
+	size_t operator()(const std::pair<unsigned long, unsigned long> & p) const
+	{
+		std::size_t hash = 0;
+
+		size_t h1 = std::hash<unsigned long>()(p.first);
+		size_t h2 = std::hash<unsigned long>()(p.second);
+
+		hash = h1 ^ (h2 << 1);
+
+		return hash;
+	}
+};
+}
 
 class Solution
 {
+	private:
+		unsigned long limit;
+		std::vector<bool> is_prime;
+		std::vector<std::vector<unsigned long>> prime_groups;
+		std::unordered_set<std::pair<unsigned long, unsigned long>> group_set;
 	public:
+		Solution(): limit(1e4) {};
+
+		void init_groups();
+
+		bool is_prime_pair(unsigned long a, unsigned long b);
+		bool is_prime_group(std::vector<unsigned long>);
 		void answer();
 
 };
