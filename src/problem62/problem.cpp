9514575d424d328583ab1b614b10849866994b36
dax <dax26d@gmail.com>
2024-10-01 19:10:50 +0800

    完成问题62

--- src/problem62/problem.cpp
+++ src/problem62/problem.cpp
@@ -5,8 +5,33 @@ https://projecteuler.net/problem=62
 dax 2024-10-01 18:28:36
 */
 #include "problem.h"
+#include <utils.h>
+#include <unordered_map>
+#include <string>
+#include <algorithm>
 
 void Solution::answer()
 {
-	print("The answer is:");
+	std::unordered_map<std::string, std::vector<unsigned long>> cubic_map;
+	unsigned long n = 0;
+
+	while(true)
+	{
+		++n;
+
+		auto cubic = n * n * n;
+
+		auto ns = std::to_string(cubic);
+		std::sort(ns.begin(), ns.end());
+
+		cubic_map[ns].push_back(cubic);
+
+		if(cubic_map[ns].size() == 5)
+		{
+			print("The answer is:", cubic_map[ns].front());
+			return;
+		}
+
+	}
+
 }
