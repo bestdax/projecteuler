5febeeab8a9730927e82013c41f2f79bb2793c0b
dax <dax26d@gmail.com>
2024-10-01 19:48:13 +0800

    完成问题63

--- src/problem63/problem.cpp
+++ src/problem63/problem.cpp
@@ -5,9 +5,23 @@ https://projecteuler.net/problem=63
 dax 2024-10-01 19:12:13
 */
 #include "problem.h"
-#include <utils.h>
+#include <bigint.h>
 
 void Solution::answer()
 {
-	print("The answer is:");
+	unsigned count{};
+
+	for(BigUInt a = 1; a < 10; ++a)
+	{
+		unsigned exponent{1};
+
+		while(a.power(exponent).length() == exponent)
+		{
+			++count;
+			++exponent;
+		}
+	}
+
+	print("The answer is:", count);
+
 }
