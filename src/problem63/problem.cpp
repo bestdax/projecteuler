b1e8754d1646565fbeb50b9d9ae0d492b4ab518e
dax <dax26d@gmail.com>
2024-10-01 19:15:07 +0800

    添加问题63

---
+++ src/problem63/problem.cpp
@@ -0,0 +1,13 @@
+/*
+Problem page link:
+https://projecteuler.net/problem=63
+
+dax 2024-10-01 19:12:13
+*/
+#include "problem.h"
+#include <utils.h>
+
+void Solution::answer()
+{
+	print("The answer is:");
+}
