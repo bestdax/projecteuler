3d9682f8de19f17ba81dafe39d7542e851df0064
dax <dax26d@gmail.com>
2024-10-02 21:37:40 +0800

    完成问题66

--- src/problem66/problem.cpp
+++ src/problem66/problem.cpp
@@ -5,8 +5,77 @@ https://projecteuler.net/problem=66
 dax 2024-10-02 16:53:47
 */
 #include "problem.h"
+#include <bigint.h>
+
+SqrtCF::SqrtCF(int n)
+{
+	int root = std::sqrt(n);
+
+	if(root * root == n) return;
+
+	int coefficient = root;
+	std::pair<int, int> pair{0, 1};
+
+	while(not remainders.contains(pair))
+	{
+		remainders.insert(pair);
+		coefficients.push_back(coefficient);
+
+		// generate next item
+		std::pair<int, int> new_pair;
+		new_pair.first = coefficient * pair.second - pair.first;
+		new_pair.second = (n - new_pair.first * new_pair.first) / pair.second;
+
+		coefficient = (root + new_pair.first) / new_pair.second;
+		pair = new_pair;
+	}
+
+}
+
+int SqrtCF::period()
+{
+	return coefficients.size() - 1;
+}
 
 void Solution::answer()
 {
-	print("The answer is:");
+	BigUInt maxp{};
+	unsigned maxd{};
+
+	for(unsigned d = 1; d <= 1000; ++d)
+	{
+		SqrtCF cf(d);
+
+		if(cf.period() > 0)
+		{
+			std::vector<BigUInt> P{0, 1};
+			std::vector<BigUInt> Q{1, 0};
+			BigUInt p, q;
+			unsigned i = 0;
+			unsigned ai = 0;
+
+			while(p * p !=  d * q * q + 1)
+			{
+				if(i == 0)
+					ai = cf.coefficients.front();
+				else
+					ai = cf.coefficients[(i - 1) % cf.period() + 1];
+
+				p = ai * P[i + 1] + P[i];
+				q = ai * Q[i + 1] + Q[i];
+				P.push_back(p);
+				Q.push_back(q);
+				++i;
+			}
+
+			if(p > maxp)
+			{
+				maxp = p;
+				maxd = d;
+			}
+		}
+	}
+
+	print("The answer is:", maxd);
+
 }
--- src/problem66/problem.h
+++ src/problem66/problem.h
@@ -1,5 +1,45 @@
 #pragma once
 #include <utils.h>
+#include <vector>
+#include <utility>
+#include <unordered_set>
+
+struct Remainder
+{
+	int numerator;
+	int denominator;
+
+	bool operator==(const Remainder& other) const
+	{
+		return numerator == other.numerator && denominator == other.denominator;
+	}
+};
+
+namespace std
+{
+	template <>
+	struct hash<std::pair<int, int>>
+	{
+		std::size_t operator()(const std::pair<int, int> &p) const
+		{
+			std::size_t p1 = std::hash<int>()(p.first);
+			std::size_t p2 = std::hash<int>()(p.second);
+
+			return p1 ^ (p2 << 1);
+		}
+	};
+}
+
+// square root continued fraction class
+class SqrtCF
+{
+	private:
+	public:
+		std::vector<int> coefficients;
+		std::unordered_set<std::pair<int, int>> remainders;
+		SqrtCF(int);
+		int period();
+};
 
 class Solution
 {
