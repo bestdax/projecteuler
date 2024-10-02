a65dd9db7f45b1a57db793900bdf0e60736f7e0e
dax <dax26d@gmail.com>
2024-10-02 11:33:53 +0800

    完成问题64

--- src/problem64/problem.cpp
+++ src/problem64/problem.cpp
@@ -5,8 +5,51 @@ https://projecteuler.net/problem=64
 dax 2024-10-01 20:00:05
 */
 #include "problem.h"
+#include <numeric>
+
+SqrtCF::SqrtCF(int n)
+{
+	int root = std::sqrt(n);
+
+	if(root * root == n) return;
+
+	int coefficient = root;
+	Remainder remainder{-root, 1};
+
+	while(not remainders.contains(remainder))
+	{
+		remainders.insert(remainder);
+		coefficients.push_back(coefficient);
+
+		// generate next item
+		coefficient = remainder.denominator / (std::sqrt(n) + remainder.numerator);
+		int denominator = n - remainder.numerator * remainder.numerator;
+		int numerator = remainder.denominator * -remainder.numerator - coefficient * denominator;
+		auto gcd = std::gcd(denominator, remainder.denominator);
+		numerator /= gcd;
+		denominator /= gcd;
+		remainder = Remainder{numerator, denominator};
+	}
+
+	coefficients.push_back(coefficient);
+}
+
+int SqrtCF::period()
+{
+	return coefficients.size() - 1;
+}
 
 void Solution::answer()
 {
-	print("The answer is:");
+	unsigned count{};
+
+	for(int i = 1; i <= 10000; ++i)
+	{
+		int period = SqrtCF(i).period();
+
+		if(period > 0 and period % 2 == 1)
+			++count;
+	}
+
+	print("The answer is:", count);
 }
--- src/problem64/problem.h
+++ src/problem64/problem.h
@@ -1,5 +1,45 @@
 #pragma once
+#include <cmath>
 #include <utils.h>
+#include <vector>
+#include <unordered_set>
+
+struct Remainder
+{
+	int numerator;
+	int denominator;
+
+	bool operator==(const Remainder& other) const
+	{
+		return numerator == other.numerator && denominator == other.denominator;
+	}
+};
+
+namespace std
+{
+	template <>
+	struct hash<Remainder>
+	{
+		std::size_t operator()(const Remainder& r) const
+		{
+			std::size_t r1 = std::hash<int>()(r.numerator);
+			std::size_t r2 = std::hash<int>()(r.denominator);
+
+			return r1 ^ (r2 << 1);
+		}
+	};
+}
+
+// square root continued fraction class
+class SqrtCF
+{
+	private:
+		std::vector<int> coefficients;
+		std::unordered_set<Remainder> remainders;
+	public:
+		SqrtCF(int);
+		int period();
+};
 
 class Solution
 {
