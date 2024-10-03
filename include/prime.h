340f4c1bf1faa0f12d6612b38c18a001f19af451
dax <dax26d@gmail.com>
2024-10-03 11:10:32 +0800

    添加单个欧拉函数的算法

--- include/prime.h
+++ include/prime.h
@@ -8,4 +8,5 @@ bool is_prime(unsigned long);
 bool is_prime_improved(unsigned long);
 std::vector<bool> sieve_of_eratosthenes(unsigned long);
 std::vector<unsigned long> sieve_of_euler(unsigned long);
+unsigned long euler_phi(unsigned long);
 }
