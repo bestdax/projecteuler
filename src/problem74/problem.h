69807a76f91d80432a9b34e89dfe12ec7a81e9d2
dax <dax26d@gmail.com>
2024-10-06 16:47:11 +0800

    优化问题74的效率

--- src/problem74/problem.h
+++ src/problem74/problem.h
@@ -1,9 +1,14 @@
 #pragma once
 #include <utils.h>
+#include <vector>
 
 class Solution
 {
+	private:
+		std::vector<unsigned> digit_factorials;
 	public:
+		Solution();
+		unsigned long factorial_sum_of_digits(unsigned long n);
 		void answer();
 
 };
