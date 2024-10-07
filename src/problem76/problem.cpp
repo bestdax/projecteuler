f47f52d583d040238b08230133d85859b92d508c
dax <dax26d@gmail.com>
2024-10-07 15:28:44 +0800

    完成问题76

--- src/problem76/problem.cpp
+++ src/problem76/problem.cpp
@@ -5,8 +5,22 @@ https://projecteuler.net/problem=76
 dax 2024-10-07 14:25:30
 */
 #include "problem.h"
+#include <vector>
 
 void Solution::answer()
 {
-	print("The answer is:");
+	unsigned sum{100};
+	std::vector<unsigned long> dp(sum + 1);
+	dp[0] = 1;
+
+	for(unsigned i = 1; i < sum; ++i)
+	{
+		for(unsigned j = i; j <= sum; ++j)
+		{
+			dp[j] += dp[j - i];
+		}
+
+	}
+
+	print("The answer is:", dp[sum]);
 }
