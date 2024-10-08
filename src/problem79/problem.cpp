cb365cd3dbbff1b1f1fb78440c7f9913ae2b83db
dax <dax26d@gmail.com>
2024-10-08 21:29:35 +0800

    完成问题79

--- src/problem79/problem.cpp
+++ src/problem79/problem.cpp
@@ -5,8 +5,53 @@ https://projecteuler.net/problem=79
 dax 2024-10-08 19:35:30
 */
 #include "problem.h"
+#include <filesystem>
+#include <fstream>
+#include <stack>
+#include <string>
+
+// 读取文本中的密码并建图
+void Solution::read_pass()
+{
+	std::filesystem::path p = __FILE__;
+	p.replace_filename("0079_keylog.txt");
+	std::ifstream file(p);
+
+	std::string line;
+
+	while(std::getline(file, line))
+	{
+		graph[line[0]].insert(line[1]);
+		graph[line[1]].insert(line[2]);
+	}
+
+}
 
 void Solution::answer()
 {
-	print("The answer is:");
+	read_pass();
+
+	// DFS算法搜索
+	std::stack<std::string> stack;
+
+	for(auto& node : graph)
+	{
+		stack.push(std::to_string(node.first - '0'));
+
+		while(!stack.empty())
+		{
+			auto pass = stack.top();
+			stack.pop();
+
+			for(auto& next : graph[pass.back()])
+			{
+				stack.push(pass + next);
+			}
+
+			// 保证密码是所有数字的一个排列
+			if(pass.size() == graph.size())
+				print("The answer is:", pass);
+		}
+	}
+
 }
--- src/problem79/problem.h
+++ src/problem79/problem.h
@@ -1,9 +1,13 @@
 #pragma once
 #include <utils.h>
+#include <set>
 
 class Solution
 {
+	private:
+		std::unordered_map<char, std::set<char>> graph;
 	public:
+		void read_pass();
 		void answer();
 
 };
