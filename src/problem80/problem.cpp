2b3c36f40d455244ce92f04e370ffe250f6cb258
dax <dax26d@gmail.com>
2024-10-08 22:26:32 +0800

    完成问题80

--- src/problem80/problem.cpp
+++ src/problem80/problem.cpp
@@ -5,8 +5,21 @@ https://projecteuler.net/problem=80
 dax 2024-10-08 21:50:40
 */
 #include "problem.h"
+#include <bigint.h>
 
 void Solution::answer()
 {
-	print("The answer is:");
+	BigUInt sum;
+
+	for(BigUInt i(1); i <= 100; ++i)
+	{
+		auto root = i.sqrt();
+
+		if(root * root != i)
+		{
+			sum += (i * BigUInt(10).power(198)).sqrt().digital_sum();
+		}
+	}
+
+	print("The answer is:", sum);
 }
